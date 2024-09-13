4175788a723b9958bb76bf4bc835a7987c6a2c0c
kidusshoa <captainkevin2008@gmail.com>
2024-09-13 06:31:11 +0300

    i hate go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,13 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,11 +15,28 @@ var (
 )
 
 func Connect(){
-	d, err :=gorm.Open("mysql", "akhil:AxlesharmaA12A/simplerest?charset=utf8&parseTime=True&loc=Local")
-	if err != nil{
-		panic(err)
-	}
-	db = d
+	err := godotenv.Load()
+    if err != nil {
+        log.Panic("Error loading .env file")
+    }
+
+    // Retrieve the values from environment variables
+    host := os.Getenv("MYSQL_HOST")
+    user := os.Getenv("MYSQL_USER")
+    password := os.Getenv("MYSQL_PASSWORD")
+    
+
+    // Create the DSN (Data Source Name)
+    dsn := fmt.Sprintf("%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host)
+
+    // Open the connection using Gorm and MySQL driver
+    d, err := gorm.Open("mysql", dsn)
+    if err != nil {
+        log.Panic("Failed to connect to database:", err)
+    }
+
+    db = d
+    fmt.Println("Database connection successful")
 }
 
 func GetDB() *gorm.DB{
